ba505d33dc42d2b4932a1cbb187e2d67f8de0e1f
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-11-25 10:04:09 -0200

    table-page and add_resource btn

--- app/helpers/loldesign_publisher/publisher_helper.rb
+++ app/helpers/loldesign_publisher/publisher_helper.rb
@@ -6,5 +6,9 @@ module LoldesignPublisher
 
       render partial: '/layouts/loldesign_publisher/modules/title_page', locals: {title: title, show_options: show_options, save_options: save_options}
     end
+
+    def add_resource(text, path)
+      render partial: '/layouts/loldesign_publisher/modules/add_resource', locals: {text: text, path: path}
+    end
   end
 end
