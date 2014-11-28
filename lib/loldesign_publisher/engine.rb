e2fb2cf1ec99c9cce6470af362b7a1f6a60107fa
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-11-28 17:57:24 -0200

    add precompile to publisher layout

--- lib/loldesign_publisher/engine.rb
+++ lib/loldesign_publisher/engine.rb
@@ -9,6 +9,8 @@ module LoldesignPublisher
         LoldesignPublisher.root_path = app.root
 
         LoldesignPublisher.read_config()
+
+        app.config.assets.precompile += %w(loldesign_publisher/publisher.css loldesign_publisher/publisher.js)
       end
 
       config.to_prepare do
--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -6,4 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( loldesign_publisher.css loldesign_publisher.js )
-Rails.application.config.assets.precompile += %w( loldesign_publisher/publisher.css loldesign_publisher/publisher.js)
