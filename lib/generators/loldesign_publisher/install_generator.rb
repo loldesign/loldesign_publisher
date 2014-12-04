05b0fc884dc0f8fd686809966d9fb2bb454b4f27
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-12-04 11:51:07 -0200

    uncomment install generator

--- lib/generators/loldesign_publisher/install_generator.rb
+++ lib/generators/loldesign_publisher/install_generator.rb
@@ -3,13 +3,13 @@ module LoldesignPublisher
   class InstallGenerator < ::Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
-    # def create_initializer_file
-    #   copy_file "loldesign_publisher.yml", "config/loldesign_publisher.yml"
-    # end
+    def create_initializer_file
+      copy_file "loldesign_publisher.yml", "config/loldesign_publisher.yml"
+    end
 
-    # def create_simple_form
-    #   run 'bundle exec rails generate simple_form:install'
-    # end
+    def create_simple_form
+      run 'bundle exec rails generate simple_form:install'
+    end
 
     def copy_override_css
       copy_file 'override_publisher.css.scss', 'app/assets/stylesheets/loldesign_publisher/override_publisher.css.scss'
