1d7890ea4fbdf08dcafed2489de040c8cede4080
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-12-04 11:49:11 -0200

    override_publisher.css file to customize your publisher

--- lib/generators/loldesign_publisher/install_generator.rb
+++ lib/generators/loldesign_publisher/install_generator.rb
@@ -3,12 +3,16 @@ module LoldesignPublisher
   class InstallGenerator < ::Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
-    def create_initializer_file
-      copy_file "loldesign_publisher.yml", "config/loldesign_publisher.yml"
-    end
+    # def create_initializer_file
+    #   copy_file "loldesign_publisher.yml", "config/loldesign_publisher.yml"
+    # end
 
-    def create_simple_form
-      run 'bundle exec rails generate simple_form:install'
+    # def create_simple_form
+    #   run 'bundle exec rails generate simple_form:install'
+    # end
+
+    def copy_override_css
+      copy_file 'override_publisher.css.scss', 'app/assets/stylesheets/loldesign_publisher/override_publisher.css.scss'
     end
   end
 end 
