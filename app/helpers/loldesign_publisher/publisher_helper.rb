a8591a0c0f1eeefe68aa8332b0b9e34928022b50
Eduardo Zaghi <eduardozaghi@gmail.com>
2015-01-20 21:58:49 -0200

    add subtitle option to title_page

--- app/helpers/loldesign_publisher/publisher_helper.rb
+++ app/helpers/loldesign_publisher/publisher_helper.rb
@@ -3,10 +3,19 @@
 module LoldesignPublisher
   module PublisherHelper
     def title_page(title, options={})
-      save_options = {display: options[:save].present?, path: options[:save] || 'javascript://'}
-      show_options = {display: options[:show].present?, path: options[:show] || 'javascript://'}
+      save_options     = {display: options[:save].present?    , path: options[:save]     || 'javascript://'}
+      show_options     = {display: options[:show].present?    , path: options[:show]     || 'javascript://'}
+      subtitle_options = {display: options[:subtitle].present?, text: options[:subtitle] || ''             }
 
-      render partial: '/layouts/loldesign_publisher/modules/title_page', locals: {title: title, show_options: show_options, save_options: save_options}
+      render partial: '/layouts/loldesign_publisher/modules/title_page', locals: {title: title, 
+                                                                                  show_options:     show_options, 
+                                                                                  save_options:     save_options,
+                                                                                  subtitle_options: subtitle_options
+                                                                                }
+    end
+
+    def method_name
+      
     end
 
     def add_resource(text, path)
