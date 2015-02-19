0fd3395789a77dbe3a411bc7c6df8d0a35acd986
Eduardo Zaghi <eduardozaghi@gmail.com>
2015-02-19 16:37:01 -0200

    config rspec for test

--- lib/loldesign_publisher/engine.rb
+++ lib/loldesign_publisher/engine.rb
@@ -16,6 +16,13 @@ module LoldesignPublisher
       config.to_prepare do
         ApplicationController.helper(LoldesignPublisher::PublisherHelper)
       end
+
+      config.generators do |g|
+        g.test_framework      :rspec,        :fixture => false
+        g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+        g.assets false
+        g.helper false
+      end
     end
   end
 end
--- loldesign_publisher.gemspec
+++ loldesign_publisher.gemspec
@@ -23,5 +23,11 @@ Gem::Specification.new do |s|
   s.add_dependency "jquery-rails"
   s.add_dependency "jquery-ui-rails"
 
+  s.add_development_dependency "pry-rails"
+  s.add_development_dependency "rspec-rails"
+  s.add_development_dependency "capybara"
+  s.add_development_dependency "factory_girl_rails"
   s.add_development_dependency "sqlite3"
+
+  s.test_files = Dir["spec/**/*"]
 end
