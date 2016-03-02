817037acfa0ff064ab9d85e57f775f27a808bb5d
Eduardo Zaghi <eduardozaghi@gmail.com>
2016-03-02 13:25:43 -0300

    assets work for rails 4.2

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -6,3 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( loldesign_publisher.css loldesign_publisher.js )
+Rails.application.config.assets.precompile += %w( loldesign_publisher/logo.svg )
